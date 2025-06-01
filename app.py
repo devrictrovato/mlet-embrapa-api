4dc08ef219c6168ac3a3398869b453682c918953
Ricardo O. Trovato <devrictrovato@gmail.com>
2025-06-01 00:51:08 -0300

    Update poetry
    Plus with caching system

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from flasgger import Swagger
+from flask_caching import Cache
 
 from routes.production import production_routes
 from routes.processing import processing_routes
@@ -8,9 +9,16 @@ from routes.importation import importation_routes
 from routes.exportation import exportation_routes
 
 app = Flask(__name__)
+
+cache = Cache()
+app.config['CACHE_TYPE'] = 'SimpleCache'  # Pode ser Redis, Memcached, etc.
+app.config['CACHE_DEFAULT_TIMEOUT'] = 300
+cache.init_app(app)
+
 Swagger(app)
 
 @app.route('/')
+@cache.cached()
 def home():
     return render_template('index.html')
 
